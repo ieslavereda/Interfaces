d195e862643448fbb5de564f4e463cdf695fea74
jalonso <jalonso@administrador-ALDA>
2020-01-22 19:30:55 +0100

    a

--- src/classes/Test.java
+++ src/classes/Test.java
@@ -14,9 +14,29 @@ public class Test {
 
 		Persona[] personas = { p1, p2, p3, p4 };
 
-		Arrays.sort(personas, (per1,per2)->per1.getEdad()-per2.getEdad());
-		
-		
+		Arrays.sort(personas, (per1, per2) -> per1.getEdad() - per2.getEdad());
+
+		for (int i = 0; i < personas.length; i++)
+			System.out.println(personas[i]);
+
+		Arrays.sort(personas, new Comparator() {
+
+			@Override
+			public int compare(Object o1, Object o2) {
+				Persona p1 = (Persona) o1, p2 = (Persona) o2;
+
+				if (p1.getNombre().compareToIgnoreCase(p2.getNombre()) == 0) {
+					if (p1.getApellidos().compareToIgnoreCase(p2.getApellidos()) == 0) {
+						return p1.getEdad() - p2.getEdad();
+					} else {
+						return p1.getApellidos().compareToIgnoreCase(p2.getApellidos());
+					}
+				} else {
+					return p1.getNombre().compareToIgnoreCase(p2.getNombre());
+				}
+			}
+
+		});
 
 		for (int i = 0; i < personas.length; i++)
 			System.out.println(personas[i]);
